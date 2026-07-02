e2909450c1595543b73cfe04b4c3a0d66e16208d
s1f102402697 <s1f102402697@iniad.org>
2026-07-02 13:43:30 +0900

    コードを整形

--- config/production.py
+++ config/production.py
@@ -3,14 +3,12 @@ import dj_database_url
 from .settings import *
 
 DEBUG = False
-SECRET_KEY = os.environ['SECRET_KEY']
-ALLOWED_HOSTS.append(os.environ['RENDER_EXTERNAL_HOSTNAME'])
+SECRET_KEY = os.environ["SECRET_KEY"]
+ALLOWED_HOSTS.append(os.environ["RENDER_EXTERNAL_HOSTNAME"])
 
-MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
 
-DATABASES = {
-        'default': dj_database_url.config()
-}
+DATABASES = {"default": dj_database_url.config()}
 
-STATTIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATTIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
