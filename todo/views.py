b61ebc26bb181d39fe72b99828d9901978aa8b48
1f10250063 <s1f102500630@iniad.org>
2026-07-09 14:03:17 +0900

    削除機能のテストを追加(修正)

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.http import Http404
 from django.utils.timezone import make_aware
 from django.utils.dateparse import parse_datetime
@@ -39,18 +39,18 @@ def update(request, task_id):
         task = Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
         raise Http404("Task does not exist")
-    if request.method == 'POST':
-        task.title = request.POST['title']
-        task.due_at = make_aware(parse_datetime(request.POST['due_at']))
+    if request.method == "POST":
+        task.title = request.POST["title"]
+        task.due_at = make_aware(parse_datetime(request.POST["due_at"]))
         task.save()
         return redirect(detail, task_id)
 
     context = {
-        'task': task
+        "task": task
     }
     return render(request, "todo/edit.html", context)
 
-def delete(request,task_id):
+def delete(request, task_id):
     try:
         task = Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
@@ -62,7 +62,7 @@ def close(request, task_id):
     try:
         task = Task.objects.get(pk=task_id)
     except Task.DoesNotExist:
-        raise Http404("Task dose not exist")
+        raise Http404("Task does not exist")
     task.completed = True
     task.save()
     return redirect(index)
